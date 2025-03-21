ac503cd28d1c7c67cee5c78675da7ba0ccc2d657
Owarino777 <malikcheikhpro14@gmail.com>
2025-03-21 23:32:00 +0100

    🔄 Redirection vers /dashboard après connexion

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -12,15 +12,16 @@ class SecurityController extends AbstractController
     #[Route(path: '/login', name: 'app_login')]
     public function login(AuthenticationUtils $authenticationUtils): Response
     {
-        // get the login error if there is one
-        $error = $authenticationUtils->getLastAuthenticationError();
+        if ($this->getUser()) {
+            return $this->redirectToRoute('app_dashboard');
+        }
 
-        // last username entered by the user
+        $error = $authenticationUtils->getLastAuthenticationError();
         $lastUsername = $authenticationUtils->getLastUsername();
 
         return $this->render('security/login.html.twig', [
             'last_username' => $lastUsername,
-            'error' => $error,
+            'error' => $error
         ]);
     }
 
